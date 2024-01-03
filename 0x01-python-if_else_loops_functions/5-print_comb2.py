9294a894b9515d625acfdd0ef661aba0c58bd4f2
mosescode1 <emosescode@gmail.com>
2024-01-03 21:35:47 +0100

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for numbers in range(100):
-    if numbers == 99:
+    if numbers < 99:
+        print("{:02d}".format(numbers), end=", ")
+    else:
         print("{:02d}".format(numbers))
-        break
-    print("{:02d}".format(numbers), end=", ")
+
