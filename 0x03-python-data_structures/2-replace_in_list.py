f4dae10f661f10c74c59955e3049436f49764010
mosescode1 <emosescode@gmail.com>
2024-01-08 10:18:12 +0100

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
+    if idx < 0 or (idx > (len(my_list) - 1)):
         return my_list
-    elif idx > (len(my_list) - 1):
-        return my_list
-    for i in my_list:
-        if i == idx:
-            my_list[idx] = element
-            return my_list
+    my_list[idx] = element
+    return my_list
