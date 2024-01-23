90359e65c1c107ceef36e142057a8baaffe826e6
mosescode1 <emosescode@gmail.com>
2024-01-23 10:18:06 +0100

    task1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         if isinstance(value, int):
-            print(value)
+            print("{:d}".format(value))
             return True
     except Exception as e:
         return False
