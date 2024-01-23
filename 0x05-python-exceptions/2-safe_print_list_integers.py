e6215984bd6fc206c9c54d9f92274527b6e3aadb
mosescode1 <emosescode@gmail.com>
2024-01-23 11:18:51 +0100

    task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+def safe_print_list_integers(my_list=[], x=0):
+    listing = []
+    i = 0   
+    try:
+        listing = my_list[:x]
+        for element in listing:
+            
+            if isinstance(element, int):
+               print("{:d}".format(element), end="")
+               i += 1
+        print()
+        return i
+    except IndexError as e:
+        return e
