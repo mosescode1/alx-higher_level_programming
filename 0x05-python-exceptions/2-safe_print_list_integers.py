506bd9625b13055ae275d79e1d573f75847efc92
mosescode1 <emosescode@gmail.com>
2024-01-23 11:36:53 +0100

    task3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     listing = []
-    i = 0   
+    i = 0
     try:
         listing = my_list[:x]
         for element in listing:
