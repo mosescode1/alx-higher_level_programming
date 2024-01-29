358ca6d6ba4cf33be3a9d1521b256be112638f00
mosescode1 <emosescode@gmail.com>
2024-01-29 19:03:32 +0100

    task 5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -59,3 +59,6 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def __del__(self):
+        print("Bye rectangle...")
