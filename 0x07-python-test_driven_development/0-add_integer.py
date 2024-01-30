bf407c60d2bc88996510f80f6ed8af57b9b8aa2b
mosescode1 <emosescode@gmail.com>
2024-01-30 22:16:27 +0100

    first

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+def add_integer(a, b=99):
+    """
+    A function that adds Two integers
+    
+    Args:
+        a (int): First operand 
+        b (int): Secound operand
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
