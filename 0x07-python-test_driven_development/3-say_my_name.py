bd50a62386afa35eacae6cb291464bf4f008032d
mosescode1 <emosescode@gmail.com>
2024-01-31 18:16:59 +0100

    task_done

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This Module containd a Matrix divider that dives every element in Matrix if list
+    Args:
+        first_name(string): parameter first name
+        last_name(string): parameter last_name
+    """
+def say_my_name(first_name, last_name=""):
+    """This function prints out the first name and last name"""
+    if not isinstance(first_name, str): 
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print(f"My name is {first_name} {last_name}")
