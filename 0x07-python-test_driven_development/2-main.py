afafd73d3361133d9329752192862084312e99b5
mosescode1 <emosescode@gmail.com>
2024-01-31 17:38:20 +0100

    task

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -2,7 +2,7 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    (1, 2, 3),
-    [4, 5, 6]
+    [1, 2, 3],
+    [4, "hi", 6]
 ]
-print(matrix_divided(matrix, 0))
+print(matrix_divided(matrix, 2))
