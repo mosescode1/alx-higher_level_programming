bc24227841c4d9ffdd290d69e113090f40531802
mosescode1 <emosescode@gmail.com>
2024-02-05 13:58:19 +0100

    refix

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,24 +1,6 @@
-#!/usr/bin/python3
 """This Module is based on Based Geometry"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+Rectangle = __import__("9-rectangle").Rectangle
 
-class Rectangle(BaseGeometry):
-    """A rectangle class inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        """Intilizing Rectangle with width and height"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        """Returns a string reoresentatiom"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
-    
-    def area(self):
-        """Returns the area of a rectangle"""
-        return self.__width * self.__height
-    
 
 class Square(Rectangle):
     """Square class inherits from a subclass Rectangle"""
@@ -31,10 +13,3 @@ class Square(Rectangle):
     def __str__(self):
         """Returns a string reoresentatiom"""
         return f"[Square] {self.__size}/{self.__size}"
-    
-
-
-s = Square(13)
-
-print(s)
-print(s.area())
