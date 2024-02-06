e85bfa8812cd83790bef544f8fe4449ad32d3f0a
mosescode1 <emosescode@gmail.com>
2024-02-06 14:00:53 +0100

    done

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+"""A module that that appends to a text file (UTF8) and 
+    returns number of characters added
+"""
+
+
+def to_json_string(my_obj):
+    """Function that converst a python representation of 
+    an object to string
+    Args:
+        my_obj(python object) : to be serlizied
+    """
+    data = json.dumps(my_obj)
+    return data
