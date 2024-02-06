3e11322c8c218740b11e22b845e9a1b9a0613283
mosescode1 <emosescode@gmail.com>
2024-02-06 15:32:47 +0100

    done

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    A Module that returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization of an object:
+"""
+
+import json
+
+def class_to_json(obj):
+    """A funtion that serilize a class object to a json object"""
+    dict_obj = obj.__dict__
+    for key, value in dict_obj.items():
+        if isinstance(value, (int, str, dict, bool, list)):
+            dict_obj[key] = value
+        elif isinstance(value, (tuple, set)):
+            dict_obj[key] = list(value)
+        elif hasattr(value, '__dict__'):
+            dict_obj[key] = class_to_json(value)
+
+    return dict_obj
