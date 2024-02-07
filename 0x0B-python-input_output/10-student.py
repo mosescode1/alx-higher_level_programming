2e298d009c1886fb8bb5af3efebbaa8ed46c0eec
mosescode1 <emosescode@gmail.com>
2024-02-07 09:23:03 +0100

    fixed_indentation

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,22 +7,24 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """intilize new variables"""
         self.first_name = first_name
-        self.last_name = last_name
+
         self.age = age
+
+        self.last_name = last_name
 
     def to_json(self, attrs=None):
         """Return a dictionsry Representation"""
         new_dict = dict()
         dict_obj = self.__dict__
         if attrs:
-                for key, value in dict_obj.items():
-                    if key in attrs:
-                        if isinstance(value, (int, bool, str, dict, list)):
-                            new_dict[key] = value
-                        elif isinstance(value, (tuple, list)):
-                            new_dict[key] = list(value)
-                        elif hasattr(value, '__dict__'):
-                            new_dict = self.to_json(value)
-                return new_dict
-            
+            for key, value in dict_obj.items():
+                if key in attrs:
+                    if isinstance(value, (int, bool, str, dict, list)):
+                        new_dict[key] = value
+                    elif isinstance(value, (tuple, list)):
+                        new_dict[key] = list(value)
+                    elif hasattr(value, '__dict__'):
+                        new_dict = self.to_json(value)
+            return new_dict
+
         return dict_obj
