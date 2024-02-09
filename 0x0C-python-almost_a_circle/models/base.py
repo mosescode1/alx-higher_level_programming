8570985e199095a960b9c0d300c49f6b29133c26
mosescode1 <emosescode@gmail.com>
2024-02-09 12:14:00 +0100

    implemented base class:

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+"""
+    This is a modile a python package
+"""
+
+
+class Base:
+    """
+        Base class useed throughout the task
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
