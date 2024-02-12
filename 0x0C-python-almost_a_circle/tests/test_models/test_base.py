c399d174415bef017434bea1f23d20c2d0442f26
mosescode1 <emosescode@gmail.com>
2024-02-12 13:14:18 +0100

    made changes

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
 import unittest
 
 from models.base import Base
 
 
 class Test_almost_a_circle(unittest.TestCase):
-    def test_base(self):
-        self.assertEqual(Base(), 1)
+    instance = Base()
+
+    def test_id(self):
+        self.assertEqual(isinstance.id, 1)
