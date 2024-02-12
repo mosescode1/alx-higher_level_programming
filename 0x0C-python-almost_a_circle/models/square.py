4a320319e7f7684a941d5efc56e411fd0542c644
mosescode1 <emosescode@gmail.com>
2024-02-12 19:08:24 +0100

    done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,11 +4,15 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """A square class a grandchild to base and a child to rectangle"""
+
     def __init__(self, size, x=0, y=0, id=None):
+        """Square class constructor"""
         super().__init__(id=id, width=size, height=size, x=x, y=y)
         self.size = size
 
     def __str__(self):
+        """String representation"""
         return "[{}] ({}) {}/{} - {}".format(
             'Square',
             self.id,
@@ -19,10 +23,20 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """returns the size"""
         return self.__width
 
     @size.setter
     def size(self, width_value):
+        """_summary_
+
+        Args:
+            width_value (int): interger value of size
+
+        Raises:
+            TypeError:  if an int isnt entred 
+            ValueError:if size value is lesser than 0
+        """
         if not isinstance(width_value, int):
             raise TypeError("width must be an integer")
         elif width_value <= 0:
@@ -31,6 +45,8 @@ class Square(Rectangle):
             self.__width = width_value
 
     def update(self, *args, **kwargs):
+        """updates the instances attributes
+        """
         if args:
             if len(args) == 1:
                 self.id = args[0]
@@ -51,6 +67,11 @@ class Square(Rectangle):
                 self.y = kwargs['y']
 
     def to_dictionary(self):
+        """_summary_
+
+        Returns:
+            dict_: a dict of all instnces attribuues
+        """
         diction = dict()
         diction['id'] = self.id
         diction['size'] = self.size
