175fc43ee221c0fbbcd9747d73cc205a5f82e452
mosescode1 <emosescode@gmail.com>
2024-03-14 10:28:50 +0100

    create-user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- script that creates the MySQL server user user_0d_
 CREATE USER IF NOT EXISTS user_0d_1@localhost
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON user_0d_1.** TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
