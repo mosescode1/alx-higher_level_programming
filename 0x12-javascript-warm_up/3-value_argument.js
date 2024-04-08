8d9c19f4c7f5bd37fc7ec69432daf38ab35f596b
mosescode1 <emosescode@gmail.com>
2024-04-08 23:27:54 +0100

    printing the first argument passed to the command line

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,15 @@
 #!/usr/bin/node
 const cmdLineArgv = process.argv;
-if (cmdLineArgv.length === 2) {
+
+function arrayLength(array) {
+  let count = 0;
+  array.forEach((element) => {
+    count++;
+  });
+  return count;
+}
+let arrayCount = arrayLength(cmdLineArgv);
+if (arrayCount === 2) {
   console.log('No argument');
 } else {
   console.log(cmdLineArgv[2]);
