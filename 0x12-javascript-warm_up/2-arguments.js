08058b03dd0aac8fa54aa6e0b694d622f43def60
mosescode1 <emosescode@gmail.com>
2024-04-08 23:10:37 +0100

    fixed requirement issue

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node;
-if (process.argv.length == 2) {
+const argvLength = process.argv.length;
+
+if (argvLength === 2) {
   console.log('No argument');
-} else {
+} else if (argvLength === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
