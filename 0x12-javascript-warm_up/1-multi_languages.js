7790f2b931ea521077b3c3793a81c86a86a9bbbc
mosescode1 <emosescode@gmail.com>
2024-04-08 21:55:24 +0100

    A script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const cLanguage = 'C is fun';
+const pyLanguage = 'Python is cool';
+const JavaScriptLanguage = 'JavaScript is amazing';
+console.log(cLanguage);
+console.log(pyLanguage);
+console.log(JavaScriptLanguage);
