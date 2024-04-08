8bb71bd30d52b9796fa02b91ffcbefdf1fb3a0e9
mosescode1 <emosescode@gmail.com>
2024-04-08 23:38:32 +0100

    not yet fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const cmdLineArgv = process.argv;
 
-function arrayLength(array) {
+function arrayLength (array) {
   let count = 0;
-  for (let elem in array) {
+  for (let {} in array) {
     count++;
   }
   return count;
 }
-let arrayCount = arrayLength(cmdLineArgv);
+const arrayCount = arrayLength(cmdLineArgv);
 if (arrayCount === 2) {
   console.log('No argument');
 } else {
