741e2efb02d10404d23cd0cd246e8feeb0e93bdd
mosescode1 <emosescode@gmail.com>
2024-04-09 00:52:06 +0100

    printing a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const cmdLineArgv = process.argv[2];
+const converted = Number(cmdLineArgv);
+
+if (cmdLineArgv === undefined) {
+  console.log('Missing size');
+} else {
+  let i, j;
+  let result = '';
+  for (i = 0; i < converted; i++) {
+    for (j = 0; j < converted; j++) {
+      result += 'X';
+    }
+    console.log(result);
+    result = '';
+  }
+}
