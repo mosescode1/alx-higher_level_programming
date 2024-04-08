fedd320ac40804e81e2e7ba16e6bd969174fe585
mosescode1 <emosescode@gmail.com>
2024-04-09 00:26:37 +0100

    print c is fun depending on the number of times passed in the argv

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const cmdLineArgv = process.argv[2];
+const converted = Number(cmdLineArgv);
+if (isNaN(converted)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i;
+  for (i = 1; i <= converted; i++) {
+    console.log('C is fun');
+  }
+}
