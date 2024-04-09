b77c02bae17cbed3226ac1ce95059d306306093d
mosescode1 <emosescode@gmail.com>
2024-04-09 23:53:25 +0100

    a square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const square = require('./5-square.js');
+
+class Square extends square {
+  charPrint (value) {
+    if (arguments.length === 0) {
+      value = 'x';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(value.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
