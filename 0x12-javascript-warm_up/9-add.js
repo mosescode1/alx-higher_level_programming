59f34ca7f80d3a94a785c1998cc76032e69ee80e
mosescode1 <emosescode@gmail.com>
2024-04-09 01:06:44 +0100

    creating a function

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const cmdLineArgv = Number(process.argv[2]);
+const cmdLineArgv2 = Number(process.argv[3]);
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(cmdLineArgv, cmdLineArgv2);
