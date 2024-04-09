0253905ad3c9ee789a551681c6e7744ccef89202
mosescode1 <emosescode@gmail.com>
2024-04-09 01:37:03 +0100

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let num = Number(process.argv[2]);
+
+function fact (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 1 || n === 0) {
+    return 1;
+  }
+  return n * fact(n - 1);
+}
+const result = fact(num);
+console.log(result);
