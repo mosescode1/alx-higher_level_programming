757a392420ee2c87235b9f9a2b4dc67e6f198a39
mosescode1 <emosescode@gmail.com>
2024-04-09 02:11:49 +0100

    second biggest number in an array

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const num = process.argv;
+const newArray = [];
+function iterateArray (num) {
+  // check the length of the array
+  if (num.length === 3 || num[2] === undefined) {
+    console.log(0);
+    return;
+  }
+
+  let i;
+  // loops through and append it value to a new array
+  for (i = 2; i < num.length; i++) {
+    newArray.push(Number(num[i]));
+  }
+}
+
+// sorts the array of numbers
+function sortArray (num) {
+  const sorted = num.slice().sort((a, b) => a - b);
+  return sorted;
+}
+// returns the second bigest number in the list
+function secondBigest (sortedArray) {
+  let i, least;
+  for (i = 0; i < sortedArray.length - 1; i++) {
+    least = i;
+  }
+  console.log(sortedArray[least]);
+}
+iterateArray(num);
+const sort = sortArray(newArray);
+secondBigest(sort);
