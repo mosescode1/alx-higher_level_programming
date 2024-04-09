50085e96252f184eda0dcace0dd6403f9b78b473
mosescode1 <emosescode@gmail.com>
2024-04-09 23:04:17 +0100

    fixed

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (arguments.length <= 1) {
-      return;
-    }
-    if (w <= 0 || h <= 0) {
-      return;
-    }
     this.width = w;
     this.height = h;
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('x'.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 }
-
 module.exports = Rectangle;
