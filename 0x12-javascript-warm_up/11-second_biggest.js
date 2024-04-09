73ce6fd4f190c9641c35cf4b7fd2eae09aefaf01
mosescode1 <emosescode@gmail.com>
2024-04-09 02:17:40 +0100

    fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,33 +1,34 @@
 #!/usr/bin/node
 const num = process.argv;
 const newArray = [];
-function iterateArray (num) {
-  // check the length of the array
-  if (num.length === 3 || num[2] === undefined) {
-    console.log(0);
-    return;
+
+if (num.length === 3 || num[2] === undefined) {
+  console.log(0);
+} else {
+  function iterateArray(num) {
+    // check the length of the array
+
+    let i;
+    // loops through and append it value to a new array
+    for (i = 2; i < num.length; i++) {
+      newArray.push(Number(num[i]));
+    }
   }
 
-  let i;
-  // loops through and append it value to a new array
-  for (i = 2; i < num.length; i++) {
-    newArray.push(Number(num[i]));
+  // sorts the array of numbers
+  function sortArray(num) {
+    const sorted = num.slice().sort((a, b) => a - b);
+    return sorted;
   }
-}
-
-// sorts the array of numbers
-function sortArray (num) {
-  const sorted = num.slice().sort((a, b) => a - b);
-  return sorted;
-}
-// returns the second bigest number in the list
-function secondBigest (sortedArray) {
-  let i, least;
-  for (i = 0; i < sortedArray.length - 1; i++) {
-    least = i;
+  // returns the second bigest number in the list
+  function secondBigest(sortedArray) {
+    let i, least;
+    for (i = 0; i < sortedArray.length - 1; i++) {
+      least = i;
+    }
+    console.log(sortedArray[least]);
   }
-  console.log(sortedArray[least]);
+  iterateArray(num);
+  const sort = sortArray(newArray);
+  secondBigest(sort);
 }
-iterateArray(num);
-const sort = sortArray(newArray);
-secondBigest(sort);
