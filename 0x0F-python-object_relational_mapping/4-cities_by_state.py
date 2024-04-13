103502f65de58005137457fe1186a7159225f573
mosescode1 <emosescode@gmail.com>
2024-04-13 21:35:49 +0100

    Relational Database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""my module on MySQLdb"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    args = sys.argv
+    user = args[1]
+    password = args[2]
+    database = args[3]
+    # user_arg = args[4]
+
+    db = MySQLdb.connect(host='localhost', user=user,
+                         password=password, port=3306, database=database)
+
+    cur = db.cursor()
+    query = """SELECT cities.id, cities.name, states.name  FROM cities 
+        INNER JOIN states 
+        WHERE cities.state_id = states.id
+        ORDER BY cities.id ASC"""
+    cur.execute(query)
+    result = cur.fetchall()
+    for res in result:
+        print(res)
+
+    cur.close()
+    db.close()
