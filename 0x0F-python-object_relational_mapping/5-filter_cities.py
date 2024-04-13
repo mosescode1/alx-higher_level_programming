299e01a3dfd73e02b9abbe9eb90d54c07640f3e9
mosescode1 <emosescode@gmail.com>
2024-04-13 21:48:22 +0100

    protective of mysql injection

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""my module on MySQLdb"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    args = sys.argv
+    user = args[1]
+    password = args[2]
+    database = args[3]
+    user_arg = args[4]
+
+    db = MySQLdb.connect(host='localhost', user=user,
+                         password=password, port=3306, database=database)
+
+    cur = db.cursor()
+    try:
+        query = """SELECT * FROM cities
+            WHERE name LIKE %s"""
+        cur.execute(query, (user_arg,))
+        result = cur.fetchall()
+        for res in result:
+            print(res)
+    except MySQLdb.Error:
+        pass
+
+    cur.close()
+    db.close()
