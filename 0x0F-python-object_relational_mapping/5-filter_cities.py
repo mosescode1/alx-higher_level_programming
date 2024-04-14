47abbc97b7bd0edc9783d63fbffad27f53c1ead6
mosescode1 <emosescode@gmail.com>
2024-04-14 19:26:50 +0100

    last task on MySQLdb

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     for res in result:
         val += f'{res[0]}, '
     val = val.rstrip(', ')
-    #val = val[:-2]
+    # val = val[:-2]
     print(val)
     cur.close()
     db.close()
