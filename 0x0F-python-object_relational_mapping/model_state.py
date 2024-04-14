4b1caef1bf72dcbb107ff3b77163f0753bb77a70
mosescode1 <emosescode@gmail.com>
2024-04-14 20:01:25 +0100

    sqlalchemy first task creating a model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Base module"""
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+Base = declarative_base()
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column('id', Integer, autoincrement=True, primary_key=True)
+    name = Column('name', String(128))
+
+    def __init__(self, name):
+        self.name = name
+
+
+# Base.metadata.create_all(engine)
