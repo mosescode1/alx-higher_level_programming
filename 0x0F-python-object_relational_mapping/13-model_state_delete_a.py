532c71799dcf4485550dbc836729fc2326b3bc44
mosescode1 <emosescode@gmail.com>
2024-04-15 00:22:45 +0100

    sqlalchemy first task creating a model

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(bind=engine)
     session = Session(bind=engine)
-    # get all obj from the database order by the id in assecding order
-    result = session.query(State).filter_by(id=2).all()
-    result[0].name = 'New Mexico'
+
+    # delete all the object containing a
+    result = session.query(State).filter(State.name.like("%a%"))
+    for res in result.all():
+        session.delete(res)
     session.commit()
     session.close()
