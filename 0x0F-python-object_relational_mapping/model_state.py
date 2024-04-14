aea6e9143d0fbda7b3b8b0e4eb79d223fc78952b
mosescode1 <emosescode@gmail.com>
2024-04-14 20:06:59 +0100

    sqlalchemy first task creating a model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,13 +9,13 @@ engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
     sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
 Base.metadata.create_all(engine)
+
+
 class State(Base):
+    """State class"""
     __tablename__ = "states"
     id = Column('id', Integer, autoincrement=True, primary_key=True)
     name = Column('name', String(128))
 
     def __init__(self, name):
         self.name = name
-
-
-
