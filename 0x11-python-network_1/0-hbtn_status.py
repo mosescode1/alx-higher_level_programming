c0e54c43a294107f7edc4d14d7f1d3f53b83853f
mosescode1 <emosescode@gmail.com>
2024-04-26 15:05:04 +0100

    HTTP response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-"""A module
+"""A module for treading in an url
     """
 import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+        print("Body response:")
+        print("- type:", type(body))
+        print("- content:", body)
+        print("- utf8 content:", body.decode('utf-8'))
