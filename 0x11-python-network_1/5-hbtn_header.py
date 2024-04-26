09bef96661e4d17019843325cadda412a79f1c01
mosescode1 <emosescode@gmail.com>
2024-04-26 17:20:01 +0100

    HTTP response using requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Write a Python script that takes in a URL
     sends a request to the URL and displays
-    the value of the variable X-Request-Id 
+    the value of the variable X-Request-Id
     in the response header"""
 
 import requests
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     with requests.get(url) as response:
         # .headers returns a dictionary
-        print(response.headers["X-Request-Id"])
+        print(response.headers.get("X-Request-Id"))
