042701faf0cd62f6d744aca54d70a0524a26f1a2
mosescode1 <emosescode@gmail.com>
2024-04-26 15:49:31 +0100

    HTTP response

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,5 +15,3 @@ if __name__ == "__main__":
             print(body)
     except urllib.error.HTTPError as e:
         print("Error code", e.code)
-    except urllib.error.URLError as e:
-        print("Error code", e.code)
