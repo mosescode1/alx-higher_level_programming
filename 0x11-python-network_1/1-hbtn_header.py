e6817a23f3f35ff6988be870096fd062819bb343
mosescode1 <emosescode@gmail.com>
2024-04-26 14:53:16 +0100

    HTTP response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,11 +2,11 @@
 """Python script that takes in a URL, sends a request"""
 import urllib.request
 import sys
-URL = sys.argv
-url_name = URL[1]
 
 
 if "__main__" == __name__:
+    URL = sys.argv
+    url_name = URL[1]
     with urllib.request.urlopen(f"{url_name}") as response:
         header = response.getheader('X-Request-Id')
     print(header)
