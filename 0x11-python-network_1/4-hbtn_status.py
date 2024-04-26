dfa2c6215e16be0ae76a06e40ea740caaf174cbf
mosescode1 <emosescode@gmail.com>
2024-04-26 17:00:38 +0100

    HTTP response

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status
+    """
+import requests
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    data = requests.get(url)
+    # print(help(data))
+    print("Body response:")
+    print("\t - type:", type(data.text))
+    print("\t - content:", data.text)
