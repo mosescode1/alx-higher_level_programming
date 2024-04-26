166a3477c6babe61e4dda5e6cc81a5a898d891f0
mosescode1 <emosescode@gmail.com>
2024-04-26 19:52:50 +0100

    HTTP response using requests

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a letter and sends
+    POST request to http://0.0.0.0:5000/search_user with
+    the letter as a parameter.
+    """
+
+import sys
+import requests
+if __name__ == "__main__":
+
+    alpha = ""
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv):
+        alpha = sys.argv[1]
+
+    param = {'q': alpha}
+
+    with requests.post(url, data=param) as response:
+        try:
+            value = response.json()
+
+            if value:
+                print(f"[{value.get("id")}] {value.get('name')}")
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
