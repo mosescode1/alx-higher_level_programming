dec8151b7f58fbaf52599fca28f29cab0237b9aa
mosescode1 <emosescode@gmail.com>
2024-05-21 15:40:56 +0100

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,25 +4,25 @@ const res = require('request');
 const url = process.argv[2];
 
 res.get(url, function (err, response, body) {
-    if (err) {
-        console.error(err);
-        return;
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const arra = [];
+  const tasks = JSON.parse(body);
+
+  const obj = {};
+
+  tasks.forEach(element => {
+    if (!(element.userId in obj)) {
+      obj[element.userId] = 0;
     }
-    const arra = [];
-    const tasks = JSON.parse(body);
+  });
 
-    const obj = {};
-
-    tasks.forEach(element => {
-        if (!(element.userId in obj)) {
-            obj[element.userId] = 0;
-        }
-    });
-
-    tasks.forEach(element => {
-        if (element.completed === true) {
-            obj[element.userId] += 1;
-        }
-    });
-    console.log(obj);
+  tasks.forEach(element => {
+    if (element.completed === true) {
+      obj[element.userId] += 1;
+    }
+  });
+  console.log(obj);
 });
