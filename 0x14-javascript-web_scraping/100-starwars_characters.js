aa240d80dacb2d0ede9793f36a47d35caebd3d65
mosescode1 <emosescode@gmail.com>
2024-05-21 16:09:36 +0100

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const resq = require('request');
+const urlId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + urlId;
+
+resq.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  // gets the characters from the movie
+  const characters = JSON.parse(body).characters;
+
+  // loop through the characters and get the names
+  characters.forEach(character => {
+    resq.get(character, (err, response, body) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(JSON.parse(body).name);
+    });
+  });
+});
