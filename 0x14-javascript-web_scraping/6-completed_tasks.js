58867867c56d662d866c7a9d9dd5c50283f72a8b
mosescode1 <emosescode@gmail.com>
2024-05-21 15:40:34 +0100

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+
+const res = require('request');
+const url = process.argv[2];
+
+res.get(url, function (err, response, body) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    const arra = [];
+    const tasks = JSON.parse(body);
+
+    const obj = {};
+
+    tasks.forEach(element => {
+        if (!(element.userId in obj)) {
+            obj[element.userId] = 0;
+        }
+    });
+
+    tasks.forEach(element => {
+        if (element.completed === true) {
+            obj[element.userId] += 1;
+        }
+    });
+    console.log(obj);
+});
