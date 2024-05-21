9a61f5de6112bf9755a4d42ad6455b518bd2c4fd
mosescode1 <emosescode@gmail.com>
2024-05-21 12:46:14 +0100

    get total movies

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const res = require('request');
+const movieId = process.argv[3];
+const url = process.argv[2] + '/' + movieId;
+const characterId = 18;
+
+res.get(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const characterUrl = JSON.parse(body).characters.filter(x => x.includes(characterId))[0];
+
+  res.get(characterUrl, (err, response, body) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const filmLength = JSON.parse(body).films.length;
+    console.log(filmLength);
+  });
+});
