2c236dd2c611f39842c44dc4213e23d67504a438
mosescode1 <emosescode@gmail.com>
2024-05-21 12:56:17 +0100

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const res = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const resPath = process.argv[3];
+
+res.get(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  fs.writeFile(resPath, body, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
