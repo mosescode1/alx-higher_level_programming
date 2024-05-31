3750b6c53cf92a604332ee28bff11aff42920382
mosescode1 <emosescode@gmail.com>
2024-05-31 10:18:52 +0100

    6-script.js

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 $(document).ready(() => {
-    $('DIV#toggle_header').click(function () {
-      $('header').toggleClass('red green');
-    });
+  $('DIV#toggle_header').click(function () {
+    $('header').toggleClass('red green');
   });
+});
